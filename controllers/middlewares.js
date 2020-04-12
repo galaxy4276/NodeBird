e09af6a106190f09a61cfd60bc1e62b1e2743beb
galaxy4276 <galaxyhi4276@gmail.com>
2020-04-12 23:32:44 +0900

    인증관련 middleware 추가

---
+++ controllers/middlewares.js
@@ -0,0 +1,13 @@
+import routes from '../routes';
+
+
+export const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) next();
+  else res.status(403).send('로그인 필요');
+};
+
+export const isNotLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) next();
+  else res.redirect(routes.root);
+};
+
