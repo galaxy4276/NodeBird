85390623bf4a1285624064a488f29c6745cdc4ed
galaxy4276 <galaxyhi4276@gmail.com>
2020-04-13 00:03:38 +0900

    인증 Controller 작성

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import routes from '../routes';
 import { join, userProfile, root } from '../controllers/pageController';
+import { isLoggedIn, isNotLoggedIn } from '../controllers/middlewares';
 
 const pageRouter = express.Router();
 
 
 pageRouter.get(routes.root, root);
-pageRouter.get(routes.profileDetail, userProfile);
-pageRouter.get(routes.join, join);
+pageRouter.get(routes.profileDetail, isLoggedIn, userProfile);
+pageRouter.get(routes.join, isNotLoggedIn, join);
 
 
 export default pageRouter;
