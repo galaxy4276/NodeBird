c04a0dfcf717a66fd553377fc49613ee4b11b2c3
galaxy4276 <galaxyhi4276@gmail.com>
2020-04-17 02:13:21 +0900

    multer 작성

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## NodeBird SNS Clone 스터디
 * Node.js 교과서 기반으로 MVC패턴으로 변경하고 바벨을 추가하여 재작성해보기.
 
-
+* 함정 발견 : module.exports 와 export default 는 절대 같지 않음. ( 에러 주의 )
+* 
---
+++ routes/postRouter.js
@@ -0,0 +1,62 @@
+import express, { Router } from 'express';
+import multer from 'multer';
+import fs from 'fs';
+import { Post, Hashtag, User } from '../models';
+import { isLoggedIn } from '../controllers/middlewares';
+import path from 'path';
+import { hash } from 'bcrypt';
+
+const postRouter = express.Router();
+
+
+fs.readdir('uploads', err => {
+  if (err) {
+    console.log('uploads 폴더가 없어 생성합니다.');
+    fs.mkdirSync('uploads');
+  }
+});
+
+const upload = multer({
+  dest: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname); // originalname 사용자가 업로드한 파일 명
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+postRouter.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
+  console.log(req.file);
+  res.json({ url: `img/${req.file.filename}`});
+});
+
+const upload2 = multer();
+
+postRouter.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      img: req.body.url,
+      userId: req.user.id,
+    });
+    console.log(post);
+    const hashtags = req.body.content.match(/#[^\s#]*/g);
+    if (hashtags) {
+      const result = await Promise.all(hashtags.map(tag => Hashtag.findOrCreate({
+        where: { title: tag.slice(1).toLowerCase() },
+      })));
+      await post.addHashtags(result.map(r => r[0]));
+    }
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+
+export default postRouter;
